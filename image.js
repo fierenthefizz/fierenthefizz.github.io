1ecf5d8cb9b3ad11f3bfc5b436c75f63614007eb
fierenthefizz <fizzyinresearch@gmail.com>
2020-06-11 13:48:38 -0300

    "Changing index for tmp files"

--- image.js
+++ image.js
@@ -1,11 +1,4 @@
-	var rand=Math.random();
-	var totalCount=21;
-	var num=Math.ceil(rand * totalCount);
-	var url=`url("images/${num}.jpg")`;
-	document.body.style.backgroundImage=url;
-	//alert(document.body.style.backgroundImage)
-	var head=document.getElementsByTagName('head')[0];
-	var link=document.createElement('link');
-	link.href=`css/jpgstyle${num}.css`;
-	link.rel='stylesheet';
-	head.append(link);
+var totalCount=21;
+var num=Math.ceil(Math.random() * totalCount);
+var url=`url("images/${num}.jpg")`;
+document.body.style.backgroundImage=url;
--- tmp.js
+++
@@ -1,4 +0,0 @@
-var totalCount=21;
-var num=Math.ceil(Math.random() * totalCount);
-var url=`url("images/${num}.jpg")`;
-document.body.style.backgroundImage=url;
